a82a288902752c5d4bec7900225b600cfe077116
michael-doll <MWDoll88@gmail.com>
2019-11-04 10:49:26 -0500

    save work

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,12 +1,25 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import com.sun.javafx.image.IntPixelGetter;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] array3 = new Integer[array1.length + array2.length];
+        System.arraycopy(array1,0,array3,0,array1.length);
+        System.arraycopy(array2,0,array3,array1.length,array2.length);
+        return array3;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
-        return null;
+        Integer[] tempArrEnd = new Integer[index];
+        Integer[] tempArrBeg = new Integer[array.length - index];
+        for(int i =0; i < tempArrEnd.length;i++){
+            tempArrEnd[i] = array[i];
+        }
+        for(int i =0; i < tempArrBeg.length;i++){
+            tempArrBeg[i] = array[tempArrEnd.length+i];
+        }
+        return merge(tempArrBeg,tempArrEnd);
     }
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
