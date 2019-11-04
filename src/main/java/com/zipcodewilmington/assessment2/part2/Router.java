ac9c35cf0cf0d77cf19002cc11843fb53e4fae0c
michael-doll <MWDoll88@gmail.com>
2019-11-04 13:51:30 -0500

    save work

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,22 +1,27 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Router {
-
+    Map<String,String> ourMap = new HashMap<>();
     public void add(String path, String controller) {
-
+        ourMap.put(path,controller);
     }
 
     public Integer size() {
-        return null;
+        return ourMap.size();
     }
 
     public String getController(String path) {
-        return null;
+        return ourMap.get(path);
     }
 
     public void update(String path, String studentController) {
+        ourMap.put(path,studentController);
     }
 
     public void remove(String path) {
+        ourMap.remove(path);
     }
 }
