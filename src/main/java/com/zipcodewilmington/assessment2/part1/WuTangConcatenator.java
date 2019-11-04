7ecce4f56c929d7be170b178ffd6101d892ea092
michael-doll <MWDoll88@gmail.com>
2019-11-04 10:18:27 -0500

    save work, part1 WuTangConcatenator

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,18 +1,18 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class WuTangConcatenator {
-    public WuTangConcatenator(Integer input) {
-    }
 
-    public Boolean isWu() {
-        return null;
-    }
+    private final Integer input;
+
+    public WuTangConcatenator(Integer input) { this.input = input; }
+
+    public Boolean isWu() { return input %3 ==0; }
 
     public Boolean isTang() {
-        return null;
+        return input %5==0;
     }
 
     public Boolean isWuTang() {
-        return null;
+        return input %15==0;
     }
 }
